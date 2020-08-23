ca4468785ade9cc948f61b00f2b4e7b04062da18
jtv3000 <60490523+spyder3000@users.noreply.github.com>
2020-08-23 19:39:24 -0400

    Add server.js

---
+++ server/server.js
@@ -0,0 +1,19 @@
+// very basics of getting an Express server -- expressjs.com 
+//  we will run this server from the command line via > node server/server.js
+
+const path = require('path'); 
+const express = require('express'); 
+const app = express();      // app is an express application
+const publicPath = path.join(__dirname, '..', 'public');  // pieces together all elements of path;  '..' to go up a folder;  
+// const port = process.env.PORT || 3000; 
+
+// express.static() will return a function
+app.use(express.static(publicPath));   // use this to register some middleware (this gives access to public folder for Express app)
+
+app.get('*', (req, res) => {     // '*' to match all unmatched routes;  this fixes 'Cannot GET /create' error of localhost:3000/create <Refresh> 
+    res.sendFile(path.join(publicPath, 'index.html'));  // if asset requested is unavail, then send to index.html   
+});        
+
+app.listen(3000, () => {        // can see this via localhost:3000  
+    console.log('Server is Running'); 
+}); 
